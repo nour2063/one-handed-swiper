3d66905f7715d718b7379b5cabff7bbcb31f1743
nour2063 <2660369s@student.gla.ac.uk>
2024-03-07 17:59:16 +0000

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,51 @@
+import { register } from "swiper/element/bundle";
+import {useEffect, useRef} from "react";
+import { motion } from "framer-motion";
+register();
+
+export function Nav({keyCount, clickEvent}) {
+
+    const swiperRef = useRef(null);
+
+    useEffect(() => {
+        const swiperContainer = swiperRef.current;
+        const params = {
+            navigation: true,
+            injectStyles: [
+                `
+                  .swiper-button-next,
+                  .swiper-button-prev {
+                    background-color: white;
+                    border-radius: 2em;
+                    width: 4em;
+                    height: 2em;
+                    padding: 1em 0;
+                    top: 3.5em;
+                `,
+            ],
+        };
+
+        Object.assign(swiperContainer, params);
+        swiperContainer.initialize();
+    }, []);
+
+    let keys = [];
+    for (let i = 0; i < keyCount; i++) {
+        keys.push(
+            <swiper-slide>
+                <motion.div
+                    className={"pill"}
+                    whileHover={{scale: 1.3}}
+                    whileTap={{scale: 0.85}}
+                    animate={{opacity: 1}}
+                    onClick={clickEvent}
+                ></motion.div>
+            </swiper-slide>
+        );
+    }
+    return (
+        <swiper-container thumbs-swiper={".my-thumbs"} class={"nav"} init="false" ref={swiperRef}>
+            {keys}
+        </swiper-container>
+    );
+}
